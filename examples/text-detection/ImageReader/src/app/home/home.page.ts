9bd883aeae8e423cb37366f3f66ed3aabd01831d
vennela <vennelakodali@gmail.com>
2020-03-03 14:03:25 -0600

    display text detections

--- examples/text-detection/ImageReader/src/app/home/home.page.ts
+++ examples/text-detection/ImageReader/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 import { Plugins, CameraSource, CameraResultType, CameraPhoto } from '@capacitor/core';
 const { CapML, Camera } = Plugins;
@@ -9,8 +9,14 @@ const { CapML, Camera } = Plugins;
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild('svgContainer', { static: false})
+  svgContainer: ElementRef<SVGElement>;
+
   private imageFile?: CameraPhoto;
   private textDetections: TextDetection[];
+  private text: string;
+  private scaleX = 300;
+  private scaleY = 300;
 
   constructor() {}
 
@@ -22,23 +28,46 @@ export class HomePage implements OnInit {
       resultType: CameraResultType.Uri,
       source: CameraSource.Photos,
     })
-    console.log(this.imageFile.webPath)
 
     var response = await CapML.detectText({filename: this.imageFile.path!})
+    this.textDetections = response.detectedText;
 
-    // received Object to array
-    response = Object.values(response.detectedText)
-    // deserialize values in the array
-    this.textDetections = response.map((textDetection: string) => <TextDetection>JSON.parse(textDetection))
+    this.drawTextLocationsOnImage();
+  }
 
-    console.log(JSON.stringify(this.textDetections));
+  drawTextLocationsOnImage() {
+    const svgContainer = this.svgContainer.nativeElement;
+    this.clearPrevDetections(svgContainer);
+
+    this.textDetections.forEach((detection: TextDetection) => {
+      // how about tilted rectangles
+      let width = (detection.bottomRight[0] - detection.bottomLeft[0]) * this.scaleX
+      let height = (detection.topLeft[1] - detection.bottomLeft[1]) * this.scaleY
+
+      let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+      rect.setAttribute("fill", "#000");
+      rect.setAttribute('x', (detection.bottomLeft[0] * this.scaleX).toString());
+      rect.setAttribute('y', (this.scaleY - (detection.topLeft[1] * this.scaleY)).toString());
+      rect.setAttribute('width', width.toString());
+      rect.setAttribute('height', height.toString());
+      rect.setAttribute('opacity', '0.4')
+      rect.onclick = () => this.text = detection.text;
+      svgContainer.appendChild(rect);
+    })
+
+  }
+
+  clearPrevDetections(svgContainer: SVGElement) {
+    while (svgContainer.childNodes.length > 1) {
+      svgContainer.removeChild(svgContainer.lastChild);
+    }
   }
 }
 
 interface TextDetection {
-  bottomLeft: string;
-  bottomRight: string;
-  topLeft: string;
-  topRight: string;
+  bottomLeft: number[];
+  bottomRight: number[];
+  topLeft: number[];
+  topRight: number[];
   text: string;
 }
