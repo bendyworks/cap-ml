41879d687d7f4983f977362f7f2799759e45f0eb
vennela <vennelakodali@gmail.com>
2020-03-03 10:39:00 -0600

    Add iOS to sample app

--- examples/text-detection/ImageReader/src/app/home/home.page.ts
+++ examples/text-detection/ImageReader/src/app/home/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
 
+  ngOnInit() {
+    console.log('hi')
+  }
 }
